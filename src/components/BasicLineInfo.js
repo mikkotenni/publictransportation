304e4af892012cc96efedf67fb418b4d031dfba9
Mikko Tenni <mikko.tenni@hotmail.com>
2021-07-31 11:26:58 +0300

    Refactor BasicLineInfo component

--- src/components/BasicLineInfo.js
+++ src/components/BasicLineInfo.js
@@ -1,15 +1,20 @@
 import React from "react";
 
+const iconMap = new Map([
+  ['b', 'fas fa-bus'],
+  ['m', 'fas fa-subway'],
+]);
+
 /*
-* Component capable of rendering line icon, name and other basic information.
+* Component capable of rendering line icon, name and whatnot.
 */
 function BasicLineInfo(props) {
+  const iconClasses = `${iconMap.get(props.data.type)} mr2`;
+
   return (
     <header className="flex items-center">
       <div className="pv2 ph3 mr4 white br2" style={{ backgroundColor: `#${props.data.color}` }}>
-        {/* FIXME: If there ever will be more types, provide proper icon. */}
-        {props.data.type === 'b' ? <i className="fas fa-bus mr2"></i> : <i className="fas fa-subway mr2"></i>}
-        {props.data.id}
+        <i className={iconClasses}></i> {props.data.id}
       </div>
       <h4>{props.data.name}</h4>
     </header>
