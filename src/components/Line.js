1fc04ed147c6fede5d07b9f573e8f5ae3a507edc
Mikko Tenni <mikko.tenni@hotmail.com>
2021-07-31 10:58:20 +0300

    Refactor Line component

--- src/components/Line.js
+++ src/components/Line.js
@@ -6,17 +6,19 @@ import BasicLineInfo from "./BasicLineInfo";
 * Component displays line information as how it's seen in list view.
 */
 function Line(props) {
+  const haltsList = props.data.halts.map((h, index) => {
+    return (
+      <span key={`${index}${h.name}`} className="dib mr2">
+        {h.name}
+        {index < props.data.halts.length - 1 && <i className="fas fa-angle-right ml1 v-mid f6"></i>}
+      </span>
+    )
+  });
+
   return (
     <Link className="no-underline dark-gray db bg-animate bg-near-white hover-bg-white pa3 pt0 mt2 br1" style={{ flexBasis: '50%' }} to={'/line-details/' + props.data.type + '/' + props.data.id}>
       <BasicLineInfo data={props.data} />
-      {props.data.halts.map((s, index) => {
-        return (
-          <span key={index} className="dib mr2">
-            {s.name}
-            {index < props.data.halts.length - 1 && <i className="fas fa-angle-right ml1 v-mid f6"></i>}
-          </span>
-        )
-      })}
+      {haltsList}
     </Link>
   )
 }
